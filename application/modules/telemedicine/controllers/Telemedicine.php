dbec932638b1b5660676e713262cb6de1ec7e1d7
stalsarona <stalsarona@gmail.com>
2020-06-04 08:25:10 +0700

    api msh blm bisa terpanggil

--- application/modules/telemedicine/controllers/Telemedicine.php
+++ application/modules/telemedicine/controllers/Telemedicine.php
@@ -9,10 +9,34 @@ class Telemedicine extends CI_Controller {
 
 	}
 
+	public function get_cors($url)
+    {
+      
+        $ch0 	 = curl_init();
+                curl_setopt($ch0, CURLOPT_URL, $url);
+                curl_setopt ($ch0, CURLOPT_RETURNTRANSFER, 1);
+        $exec0 	 = curl_exec ($ch0);
+        curl_close ($ch0); 
+        return $exec0;
+	}
+
+	public function get_pasien()
+    {
+		$id = $this->input->post('nopas');
+		//$get_pass = $this->get_log();
+        $url = "http://api.rstugurejo.jatengprov.go.id:8000/wsrstugu_dev/rstugu/telemedicine/get_pasien/".$id."";
+        $data = json_decode($this->get_cors($url), TRUE);
+        //$data = $this->get_cors($url);
+        //untuk scraping json harus di decode baru di looping dahulu
+        $this->output->set_content_type('application/json')->set_output(json_encode($data));
+	}
+
 	public function poli()
 	{
 		$this->load->view('V_telemedicine');
 
 	}
+
+	
 
 }
